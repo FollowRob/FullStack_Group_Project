54b2a4bee3c34f334b9a2022f7376e73801bb8f6
Amelia James <ameliafjjames@gmail.com>
2026-01-05 21:05:35 +0000

    display question data

--- quizapp/views.py
+++ quizapp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, World!")
+def view_question(request):
+
+    # Placeholder until model is completed
+    question = {
+        "question": "What is the capital of France?",
+        "choice1": "Berlin",
+        "choice2": "Madrid",
+        "choice3": "Paris",
+        "choice4": "Rome",
+        "answer": "Paris",
+    }
+
+    return render(
+        request,
+        "quizapp/question.html",
+        {
+            "question": question,
+        },
+    )
