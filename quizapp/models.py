b949b68d1c6305c6c7a22df7fe5df675d74f0113
Amelia James <ameliafjjames@gmail.com>
2026-01-06 15:13:29 +0000

    Add initial quiz data models

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model):
+
+    name = models.TextField(max_length=60)
+    def __str__(self):
+        return f"Quiz app"
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.SET_NULL, related_name="questions", null=True, blank=True)
+    question_text = models.TextField(max_length=60)
+    choice_1 = models.TextField(max_length=60)
+    choice_2 = models.TextField(max_length=60)
+    choice_3 = models.TextField(max_length=60)
+    choice_4 = models.TextField(max_length=60)
+    answer = models.TextField(max_length=60)
+    
