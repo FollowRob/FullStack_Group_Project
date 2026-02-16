d4c874ce214227680fee79b1630bd77ddea63ea7
Arun Dhanjal <arundhanjal@hotmail.com>
2026-02-16 16:45:17 +0000

    Remove references to test button in JS and CSS files

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,3 @@
-// // Find and select the button from the DOM
-// let coolButton = document.getElementById('cool-button');
-// // Create a variable to hold the current colour of the button
-// let buttonColor = "orange";
-// // Update the button DOM element to match the buttonColour variable's value
-// coolButton.style.backgroundColor = buttonColor;
-
-// /**
-//  * This function will change the colour of the button, depending on the current colour.
-//  */
-// function changeColor() {
-//     // This if statement only changes the value of the buttonColor variable - it does NOT change the DOM element! That is handled after the if statement by the DOM update line.
-//     if (buttonColor === "orange") {
-//         buttonColor = "green";
-//     } else {
-//         buttonColor = "orange";
-//     }
-
-//     // This line will reassign the DOM style based on the updated buttonColor value.
-//     coolButton.style.backgroundColor = buttonColor;
-
-//     // This brings up an alert once the button is clicked. The setTimeout thing ensures that the DOM element colour gets changed before.
-//     setTimeout(() => alert("You just changed the colour!"), 0);
-
-// }
-
-// // This creates a click listener on the button, which then calls the changeColor function.
-// coolButton.addEventListener("click", changeColor);
-
 // Find and select the buttons from the DOM
 let choiceButtons = document.querySelectorAll("#question-choices button");
 let checkAnswerButton = document.getElementById("check-answer-button");
