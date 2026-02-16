08711cc7a02cdf7a99e13cc3ca80020bebb74100
Arun Dhanjal <arundhanjal@hotmail.com>
2026-02-16 16:42:39 +0000

    Add JavaScript for checking answer

--- static/js/script.js
+++ static/js/script.js
@@ -29,8 +29,9 @@
 
 // Find and select the buttons from the DOM
 let choiceButtons = document.querySelectorAll("#question-choices button");
+let checkAnswerButton = document.getElementById("check-answer-button");
 
-let selectedChoice
+let selectedChoice = "";
 
 // Function to retrieve specific choice of the selected button
 let selectChoice = (e) => {
@@ -38,4 +39,19 @@ let selectChoice = (e) => {
     alert(selectedChoice);
 };
 
+// Function to check selected choice against actual answer
+let checkAnswer = (e) => {
+    let answer = e.target.getAttribute("data-correct-answer");
+    
+    if (selectedChoice === "") {
+        alert("You didn't select a choice!");
+    } else if (selectedChoice === answer) {
+        alert("You got it right!");
+    } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+    }
+};
+
+// Add event listeners
 choiceButtons.forEach(button => button.addEventListener("click", selectChoice));
+checkAnswerButton.addEventListener("click", checkAnswer);
